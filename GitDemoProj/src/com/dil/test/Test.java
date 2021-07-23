d4acfbd9ce9fcb869445967d48e127f2750cb60b
Dev1 <Dev1@gmail.com>
2021-07-23 15:33:45 +0530

    Test.java modified by Dev1

--- GitDemoProj/src/com/dil/test/Test.java
+++ GitDemoProj/src/com/dil/test/Test.java
@@ -1,5 +1,11 @@
 package com.dil.test;
 
+import com.dil.util.Utils;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utils u = new Utils();
+		System.out.println(u.getSysDate());
+	}
 }
