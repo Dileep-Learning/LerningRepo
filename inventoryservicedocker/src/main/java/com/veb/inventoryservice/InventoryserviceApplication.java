4136130cc00047187e09f781a1c253f1489e879f
Dev1 <Dev1@gmail.com>
2021-07-30 20:33:24 +0530

    App changed

--- inventoryservicedocker/src/main/java/com/veb/inventoryservice/InventoryserviceApplication.java
+++ inventoryservicedocker/src/main/java/com/veb/inventoryservice/InventoryserviceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 //@EnableEurekaClient
+
 public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
